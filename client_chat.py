f657d79d1165869a2b0c09051c6078377512ffcc
azam2020 <azam8933033@gmail.com>
2024-01-19 17:54:50 +0530

    hh

--- client_chat.py
+++ client_chat.py
@@ -1,22 +1,27 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect(('192.168.230.234',5556))
-choice = int(input(''' What do you want?   
+client_socket.connect(('192.168.230.234',5557))
+choice = input(''' What do you want?   
 		   1. Run a Command
-	           2. Chatting  '''))
-if choice !=1 | choice!=2:
+	           2. Chatting  ''')
+c = int(choice)
+
+if c not in range(1,3):
 	print("Invalid Choice")
 	client_socket.close()
+
 client_socket.send(choice.encode())
 while True:
-	if choice==1:
-		command = input("Enter a command")
+	if c==1:
+		command = input("Enter a command: ")
 		client_socket.send(command.encode())
+		if command=="bye":
+			break
 		result = client_socket.recv(1000).decode()
 		print("Result: ")
 		print(result)
-	elif choice==2:
+	elif c==2:
 		message = input("Type message: ")
 		client_socket.send(message.encode())
 		reply = client_socket.recv(1000).decode()
--- server_chat.py
+++ server_chat.py
@@ -1,16 +1,18 @@
 import socket
 import subprocess
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_socket.bind(('',5556))
+server_socket.bind(('',5557))
 server_socket.listen(5)
 print("Server Side:")
 client_socket, addr = server_socket.accept()
 choice = client_socket.recv(1000).decode()
-#choice = int(choice)
+choice = int(choice)
 while True:
 	if choice == 1:
 		command = client_socket.recv(1000).decode()
 		print(f"command received: {command}")
+		if command=="bye":
+			break
 		result = subprocess.check_output([command])
 		result = result.decode('utf-8')
 		client_socket.send(result.encode())
